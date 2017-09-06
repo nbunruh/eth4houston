f65214cb0cae33321025db1ce77ab0095312a7e0
server <nolanbunruh@gmail.com>
2017-09-06 16:01:00 -0700

    add team page, added navbar,

--- assets/js/helphouston.js
+++ assets/js/helphouston.js
@@ -17,5 +17,24 @@
 
 })(jQuery); // End of use strict
 
+// Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 54
+  });
+
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  });
 
 }
